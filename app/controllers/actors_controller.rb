ce33847c364fffcb2860172b9d9770958b1ebb9a
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-30 19:44:31 +0000

    actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,6 +8,9 @@ class ActorsController < ApplicationController
     @id_num = params[:id].to_i
     @a = Actor.where({:id => @id_num}).at(0)
 
+    @c = Character.where(:actor_id => @id_num).all()
+    
+
     render({ :template => "actors_templates/bios"})
   end
 
